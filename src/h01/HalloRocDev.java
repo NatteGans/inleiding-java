f6bcc0f291a57ccdf6cb80348ab7272b9b3b3e90
Chakir Iben El Farok <chakiribenelfarok@MacBook-Air-van-Chakir.local>
2019-09-04 09:13:25 +0200

    My commit

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 8A!");
+        System.out.println("Koude kaas is niet lekker!");
     }
 }
